712499d3f8a41406a1521f7678d31360bd8b1564
juniortorr <juniortorr77@gmail.com>
2024-03-02 17:01:50 -0800

    - edit project list and run build

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,4 +1,5 @@
 import Micamera from '../images/micamera-studios.png';
+import TaskApp from '../images/Tasker.jpg';
 
 class Project {
   constructor(title, subheading, description, img) {
@@ -16,23 +17,17 @@ class Project {
 const micamera = new Project(
   'Micamera Studios',
   'Photography Web App',
-  'Designed and developed a personalized, api linked application for client to funnel new interest and receive new lead information. Click on image to see live preview',
+  'Designed and developed a personalized, api linked application for client to funnel new interest and receive new lead information. Click on image to see live preview.',
   Micamera
 );
 
-const battleship = new Project(
-  'Battleship',
-  'Game App',
-  'A javascript heavy version of battleship. The AI will never make the same move twice. Click on image to see live preview',
-  Micamera
-);
 const taskApp = new Project(
   'Task Organizer',
   'Productivity App',
-  'A Javascript based app designed for keeping track of daily and weekly tasks. Click on image to see live preview',
-  Micamera
+  'A Javascript based app designed for keeping track of daily and weekly tasks. Click on image to see live preview.',
+  TaskApp
 );
 
-const projects = [micamera, battleship, taskApp];
+const projects = [micamera, taskApp];
 
 export default projects;
